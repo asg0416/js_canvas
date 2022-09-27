cdf3bf316bf12ba97579f4cdf43e1d6a664dfec8
asg0416 <asg0416@naver.com>
2022-09-27 23:17:01 +0900

    [EX] 마우스 움직임에 따라 선을 그리며, 랜덤 색상을 표현하는 예제

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
-const canvas = document.querySelector('canvas');
+const canvas = document.querySelector("canvas");
 /*
 canvas API 내부 context는 canvas element에서 2D 그림 렌더링을 위한 객체를 제공하는 인터페이스이다.
 CanvasRenderingContext2D Docs : https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
 Interface 참고 : https://hyunseob.github.io/2016/10/17/typescript-interface/
-*/ 
-const ctx = canvas.getContext('2d');
+*/
+const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.lineWidth = 2
-ctx.moveTo(0,0) // 시작 지점 설정
+const colors = [
+    "#eccc68",
+    "#ff7f50",
+    "#ff6b81",
+    "#a4b0be",
+    "#57606f",
+    "#ffa502",
+    "#ff6348",
+    "#ff4757",
+    "#747d8c",
+    "#7bed9f",
+    "#70a1ff",
+    "#2f3542",
+    "#5352ed",
+    "#2ed573",
+    "#1e90ff",
+];
+
+ctx.lineWidth = 2;
 
 const onclick = (e) => {
-    ctx.lineTo(e.offsetX, e.offsetY)
-    ctx.stroke()
-}
+    ctx.beginPath()
+    ctx.moveTo(0, 0); // 시작 지점 설정
+    const color = colors[Math.floor(Math.random() * colors.length)]
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.strokeStyle = color
+    ctx.stroke();
+};
 // 마우스를 움직일때마다 선 그리는 함수 실행
-canvas.addEventListener('mousemove', onclick)
+canvas.addEventListener("mousemove", onclick);
