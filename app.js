6bce156d31813b73d925e9d0807a273b7a83bfdd
asg0416 <asg0416@naver.com>
2022-09-27 21:46:52 +0900

    [Feat] canvas 사이즈 설정 및 사각형 그리기

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-alert("Let's start")
+const canvas = document.querySelector('canvas');
+/*
+canvas API 내부 context는 canvas element에서 2D 그림 렌더링을 위한 객체를 제공하는 인터페이스이다.
+CanvasRenderingContext2D Docs : https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
+Interface 참고 : https://hyunseob.github.io/2016/10/17/typescript-interface/
+*/ 
+const ctx = canvas.getContext('2d');
+canvas.width = 800;
+canvas.height = 800;
+
+ctx.fillRect(250, 50, 100, 200)
+ctx.rect(10, 20, 30, 30);
+ctx.rect(40, 60, 30, 30);
+ctx.rect(80, 100, 30, 30);
+ctx.fill();
